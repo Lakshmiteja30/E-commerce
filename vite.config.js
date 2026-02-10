56b94c3dd145cb7b4dad8606aeb4baee6fd4d38d
Lakshmiteja30 <dasareddylakshmiteja@gmail.com>
2026-02-10 13:16:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base:'/' ,
   plugins: [react()], 
-  base:'/ecom/' // 👈 ADD THIS LINE
 })
